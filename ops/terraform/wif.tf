42c6277537e8c12bd0c626b6412ae6b7457dd6a8
Orinda Harrison (Mitch) <mitchke@duck.com>
2025-09-02 23:28:22 +0300

    Update Workload Identity Pool resources in Terraform config
    Replace the `data` source with a managed resource for `google_iam_workload_identity_pool`. Update pool and provider IDs to new convention (`ga-pool-keycloak-v1` and `ga-provider-keycloak-v1`). Adjust related references to align with the new setup.

--- ops/terraform/wif.tf
+++ ops/terraform/wif.tf
@@ -1,23 +1,19 @@
 # --- Workload Identity Federation (Securely connect GitHub Actions to GCP) ---
-# resource "google_iam_workload_identity_pool" "github" {
-#   # Note: Workload Identity Pools are soft-deleted in GCP and cannot be recreated with the same ID for ~30 days.
-#   # To prevent breaking re-provisioning flows after a destroy, we forbid destroying this resource via Terraform.
-#   # If you truly need to delete the pool, remove it manually in GCP and also remove it from state explicitly.
-#   workload_identity_pool_id = "github-actions-pool-keycloak"
-#   display_name              = "GitHub Actions (Keycloak)"
-#
-#   lifecycle {
-#     prevent_destroy = true
-#   }
-# }
+resource "google_iam_workload_identity_pool" "github" {
+  # Note: Workload Identity Pools are soft-deleted in GCP and cannot be recreated with the same ID for ~30 days.
+  # To prevent breaking re-provisioning flows after a destroy, we forbid destroying this resource via Terraform.
+  # If you truly need to delete the pool, remove it manually in GCP and also remove it from state explicitly.
+  workload_identity_pool_id = "ga-pool-keycloak-v1"
+  display_name              = "GitHub Actions (Keycloak)"
 
-data "google_iam_workload_identity_pool" "github" {
-  workload_identity_pool_id = "github-actions-pool-keycloak"
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "google_iam_workload_identity_pool_provider" "github_provider" {
-  workload_identity_pool_id          = data.google_iam_workload_identity_pool.github.workload_identity_pool_id
-  workload_identity_pool_provider_id = "github-provider-keycloak"
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github.workload_identity_pool_id
+  workload_identity_pool_provider_id = "ga-provider-keycloak-v1"
   display_name                       = "GitHub Actions (Keycloak)"
   attribute_condition                = "assertion.repository=='${var.github_repo}'"
   attribute_mapping = {
@@ -37,5 +33,5 @@ resource "google_iam_workload_identity_pool_provider" "github_provider" {
 resource "google_service_account_iam_member" "workload_identity_user" {
   service_account_id = google_service_account.github_actions_sa.name
   role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/${data.google_iam_workload_identity_pool.github.name}/attribute.repository/${var.github_repo}"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository/${var.github_repo}"
 }
